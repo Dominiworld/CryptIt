e5ce8ab9eaab9100bfd23ea02db079059e594945
Dominiworld <dominiworld@gmail.com>
2016-02-22 13:11:44 +0700

    New

---
+++ CryptIt/Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptIt.Model
+{
+    public class User
+    {
+        private int Id { get; set; }
+    }
+}
