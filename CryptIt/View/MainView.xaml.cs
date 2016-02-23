ea4fc56e61729985f275d551debffd6963bbd50f
DoroninaD <d.doronina@gmail.com>
2016-02-24 01:11:00 +0700

    1

---
+++ CryptIt/View/MainView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Web;
+using System.Windows;
+using System.Windows.Navigation;
+using CryptIt.ViewModel;
+using vkAPI;
+
+namespace CryptIt.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView
+    {
+
+        public MainView()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+
+        private void Authorize(object sender, RoutedEventArgs e)
+        {
+            var model = DataContext as MainViewModel;
+            if (model!=null)
+            {
+                WebBrowser.Navigate(new Uri(model.AuthorizeUrl));
+            }         
+        }
+
+        private void WebBrowser_OnNavigated(object sender, NavigationEventArgs e)
+        {          
+            var urlParams = HttpUtility.ParseQueryString(e.Uri.Fragment.Substring(1));
+            AuthorizeService.Instance.AccessToken = urlParams.Get("access_token");
+            AuthorizeService.Instance.CurrentUserId = urlParams.Get("user_id");       
+        }
+    }
+}
---
+++ CryptIt/ViewModel/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using CryptIt.Commands;
+using vkAPI;
+
+
+
+namespace CryptIt.ViewModel
+{
+    public class MainViewModel:ViewModelBase
+    {
+        private string _authorizeUrl;
+        private MessageService _messageService;
+        private UserService _userService;
+
+        public MainViewModel()
+        {
+            _messageService = new MessageService();
+            _userService = new UserService();
+            
+            
+            AuthorizeUrl = AuthorizeService.Instance.GetAuthorizeUrl(5296011);
+            GetDialogsCommand = new DelegateCommand(GetDialogs); 
+        }
+
+        private void GetDialogs()
+        {
+           var dialogs =  _messageService.GetDialogs();
+            var messages = _messageService.GetMessages(1);
+            var friends = _userService.GetFriends(int.Parse(AuthorizeService.Instance.CurrentUserId));
+        }
+
+        public DelegateCommand GetDialogsCommand { get; set; }
+
+
+        public string AuthorizeUrl
+        {
+            get { return _authorizeUrl; }
+            set
+            {
+                _authorizeUrl = value;
+                OnPropertyChanged();
+            }
+        }
+
+   
+    }
+}
