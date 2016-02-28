2e94acdd4d2e307b01fe7ea7d6c5f8bf9c6d99e3
DoroninaD <d.doronina@gmail.com>
2016-02-28 10:59:19 +0700

    .trying to apply gitignore2

---
+++ vkAPI/BaseService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace vkAPI
+{
+    public class BaseService
+    {
+        protected JObject GetUrl(string url)
+        {
+            
+
+            using (var client = new System.Net.WebClient())
+            {
+                client.Encoding = Encoding.UTF8;
+                var response = client.DownloadString(url);
+                Console.WriteLine(response);
+                return JObject.Parse(response);
+            }
+        }
+    }
+}
---
+++ vkAPI/MessageService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using Newtonsoft.Json;
+
+namespace vkAPI
+{
+    public class MessageService:BaseService
+    {
+        public IEnumerable<MessageInfo> GetDialogs()
+        {
+            var token = AuthorizeService.Instance.AccessToken;
+            var url = "https://api.vk.com/method/messages.getDialogs?v=5.45&access_token="+token;
+            var obj = GetUrl(url);
+
+            var json =
+                JsonConvert.DeserializeObject<IEnumerable<MessageInfo>>(obj["response"]["items"].ToString());
+            return json;
+        }
+
+        public IEnumerable<Message> GetMessages(int isOutComming)
+        {
+            var token = AuthorizeService.Instance.AccessToken;
+            var url = string.Format("https://api.vk.com/method/messages.get?out={0}&v=5.45&access_token={1}",
+                isOutComming, token);
+            var obj = GetUrl(url);
+            return JsonConvert.DeserializeObject<IEnumerable<Message>>(obj["response"]["items"].ToString());
+        }
+
+    }
+}
