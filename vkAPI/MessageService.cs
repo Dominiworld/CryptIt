2d360beb6864375a56f365610c3c4ecbf06760a0
DoroninaD <d.doronina@gmail.com>
2016-03-20 19:26:49 +0700

    Added MarkMessagesAsRead function

--- vkAPI/MessageService.cs
+++ vkAPI/MessageService.cs
@@ -12,22 +12,23 @@ namespace vkAPI
 {
     public class MessageService:BaseService
     {
+        private UserService _userService;
+        public MessageService()
+        {
+            _userService = new UserService();
+        }
 
-        public delegate void GotNewMessage(Message message);
-
-        public event GotNewMessage GotNewMessageEvent;
-
-        private UserService _userService = new UserService();
-        public async Task<IEnumerable<Message> >GetDialogs()
+        public async Task<IEnumerable<Message>>GetDialogs()
         {
             var token = AuthorizeService.Instance.AccessToken;
             var url = "https://api.vk.com/method/messages.getDialogs?v=5.45&access_token="+token;
             var obj = await GetUrl(url);
             var json =
                 JsonConvert.DeserializeObject<IEnumerable<Message>>(obj["response"]["items"].ToString());
+
             foreach (var messageInfo in json.ToArray())
             {
-                messageInfo.User =await _userService.GetUser(messageInfo.UserId);
+                messageInfo.User =  await _userService.GetUser(messageInfo.UserId);
             }
             return json;
         }
@@ -56,51 +57,30 @@ namespace vkAPI
             return messages;
         }
 
-        public async void SendMessage(int userId, string message)
+        public async Task<int> SendMessage(int userId, string message)
         {
+            if (message == null)
+            {
+                return 0;
+            }
             var token = AuthorizeService.Instance.AccessToken;
 
-            var url =String.Format("https://api.vk.com/method/messages.send?v=5.45&user_id={0}&message={1}&access_token={2}",
-                userId, message,token);
+            var url =
+                $"https://api.vk.com/method/messages.send?v=5.45&user_id={userId}&message={message}&access_token={token}";
+            var id =  await GetUrl(url);
+            return JsonConvert.DeserializeObject<int>(id["response"].ToString());
+        }
+
+        public async void MarkMessagesAsRead(List<int> messageIds, int peerId)
+        {
+            string messageIdsString = string.Join(",", messageIds);
+            var token = AuthorizeService.Instance.AccessToken;
+            var url =
+                $"https://api.vk.com/method/messages.markAsRead?v=5.45&access_token={token}&peer_id={peerId}&message_ids={messageIdsString}";
             await GetUrl(url);
         }
 
-        public async void ConnectToLongPollServer(bool useSsl=true, bool needPts=true)
-        {
-            var token = AuthorizeService.Instance.AccessToken;
-
-            var url = String.Format("https://api.vk.com/method/messages.getLongPollServer?v=5.45&use_ssl={0}&need_pts={1}&access_token={2}",
-               useSsl, needPts, token);
-            var obj = await GetUrl(url);
-            var connectionSettings = JsonConvert.DeserializeObject<LongPollConnectionSettings>(obj["response"].ToString());
-
-            while (connectionSettings.TS != 0)
-            {
-                url = String.Format("http://{0}?act=a_check&key={1}&ts={2}&wait=25&mode=2",
-                connectionSettings.Adress, connectionSettings.Key, connectionSettings.TS);
-                obj = await GetUrl(url);
-               
-                var updates = JsonConvert.DeserializeObject<LongPoolServerResponse>(obj.ToString());
-                connectionSettings.TS = updates.Ts;
-                
-                foreach (var update in updates.Updates)
-                {
-                    if (int.Parse(update[0].ToString()) == 4)
-                    {
-                        var message = new Message
-                        {
-                            Body = update[6].ToString(),
-                            UserId = int.Parse(update[3].ToString()),
-                            UnixTime = int.Parse(update[4].ToString()),
-                            Out = (int.Parse(update[2].ToString()) & 2)!=0, //+2 - OUTBOX   
-                            IsNotRead = (int.Parse(update[2].ToString()) & 1)!=0 //+1 - UNREAD
-                        };
-                        
-                        if (GotNewMessageEvent != null) GotNewMessageEvent(message);
-                    }
-                }
-            }
-        }
+       
 
     }
 }
