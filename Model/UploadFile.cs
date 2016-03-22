560e37e04479b11a920255884110ee49102ccb22
DoroninaD <d.doronina@gmail.com>
2016-03-22 18:30:12 +0700

    test commit

---
+++ Model/UploadFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Annotations;
+
+namespace Model
+{
+   public  class UploadFile
+
+    {
+       public string FileName { get; set; }
+
+       public string Url { get; set; }
+
+    
+
+
+
+    }
+}
