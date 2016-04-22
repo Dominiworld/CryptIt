b4937a82c51c91cc2e635b1448cc96fda73f8142
noxed <noxed@mail.ru>
2016-04-23 01:34:30 +0700

    Update CryptTool.cs
    Added ignoring of an exception, which apeared in DecryptString function in case of not compatible rsa keys

--- CryptingTool/CryptTool.cs
+++ CryptingTool/CryptTool.cs
@@ -90,14 +90,21 @@ namespace CryptingTool
               (((dwKeySize / 8) / 3) * 4) + 4 : ((dwKeySize / 8) / 3) * 4;
             int iterations = inputString.Length / base64BlockSize;
             ArrayList arrayList = new ArrayList();
-            for (int i = 0; i < iterations; i++)
+            try
             {
-                byte[] encryptedBytes = Convert.FromBase64String(
-                     inputString.Substring(base64BlockSize * i, base64BlockSize));
-
-                Array.Reverse(encryptedBytes);
-                arrayList.AddRange(rsaCryptoServiceProvider.Decrypt(
-                                    encryptedBytes, true));
+               for (int i = 0; i < iterations; i++)
+               {
+                   byte[] encryptedBytes = Convert.FromBase64String(
+                        inputString.Substring(base64BlockSize * i, base64BlockSize));
+   
+                   Array.Reverse(encryptedBytes);
+                   arrayList.AddRange(rsaCryptoServiceProvider.Decrypt(
+                                       encryptedBytes, true));
+               }
+            }
+             catch (Exception)
+            {
+                return inputString;
             }
             return Encoding.UTF32.GetString(arrayList.ToArray(Type.GetType("System.Byte")) as byte[]);
         }
